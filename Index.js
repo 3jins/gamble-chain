419fab1aa54798dbf15740b5dd03a5c12e53ce48
Sejin Jeon <jinaidy93@gmail.com>
2018-06-19 20:33:38 +0900

    Bug fix: Index.js
    Fixed interactive cli shell.

--- Index.js
+++ Index.js
@@ -2,33 +2,30 @@
 import Node from './Node';
 import readLine from 'readline';
 
-const node = new Node('A');
+const node = new Node('A', 3);
 const rl = readLine.createInterface( process.stdin, process.stdout );
-let executionLoop = true;
 
 const promptInput = (prompt, handler) => {
     rl.question(prompt, input => {
-        if (handler(input) !== false)
+        if(handler(input) !== false)
             promptInput(prompt, handler);
         else
             rl.close();
     });
 };
 
-while (executionLoop) {
-    promptInput('gamble chain > ', input => {
-        switch(input) {
-            case 'make':
-                node.makeGameRoom();
-                break;
-            case 'join':
-                node.join();
-                break;
-            case 'exit':
-                executionLoop = false;
-                break;
-            default:
-                console.log("There is no command \'" + input + "\'");
-        }
-    });
-}
+promptInput('gamble chain > ', input => {
+    switch(input) {
+        case 'make':
+            node.makeGameRoom();
+            break;
+        case 'join':
+            node.join();
+            break;
+        case 'exit':
+            return false;
+        default:
+            console.log("There is no command \'" + input + "\'");
+    }
+    return true;
+});
