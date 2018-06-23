fa732aa9cdbfacb3088b027e5d8852288ae16881
Sejin Jeon <jinaidy93@gmail.com>
2018-06-24 03:15:45 +0900

    Create temporary p2p mocking functions

--- Node.js
+++ Node.js
@@ -11,4 +11,30 @@ export default class Node {
         this.rsaKey = new RSAKey();
         // this.aesKey = new AESKey();
     }
+
+    /* Propagate the updated chain to all nodes
+     *
+     * 1. Query to the central server and find all nodes
+     * 2. Establish a TCP Connection to each node and send the chain to each of them.
+     *  2-1. Messages should be signed by private key of the propagating node.
+     *  2-2. Mark a lock to centralized server before the propagation, so avoid the collision, and unmark when the propagation is finished.
+     */
+    propagateChain = () => {
+        // Refer here: https://gist.github.com/tedmiston/5935757
+    };
+    propagateChainTemp = (nodeList) => {
+        const numNode = nodeList.length;
+        for(let i = 0; i < numNode; i++) {
+            nodeList[i].receiveChainTemp(this.chain);
+        }
+    };
+
+    /* Wait messages in a separate thread.
+     * When a new message arrives, update the chain property.
+     */
+    receiveChain = () => {};
+    receiveChainTemp = (chain) => {
+        if(this.chain === chain) return;
+        this.chain = chain;
+    }
 }
