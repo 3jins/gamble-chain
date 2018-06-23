172b4fa7a60b0def04e45dd1ece3f29f30236071
Sejin Jeon <jinaidy93@gmail.com>
2018-06-24 03:17:26 +0900

    Modify test.js

--- test.js
+++ test.js
@@ -1,23 +1,48 @@
 'use strict';
-import Node from './Node';
-import {checkTimeSpent} from "./utils/testBenches";
+import MiningNode from './MiningNode';
+import GamblingNode from './GamblingNode';
+import {checkTimeSpent} from "./utils/TestBenches";
 
-const nodeA = new Node('A', 3);
-const nodeB = new Node('B', 3);
+const difficulty = 3;
+const miningNodeA = new MiningNode('mA', difficulty);
+const gamblingNodeA = new GamblingNode('gA');
+const gamblingNodeB = new GamblingNode('gB');
+const nodeList = [miningNodeA, gamblingNodeA, gamblingNodeB];
 
 checkTimeSpent(
-    () => nodeA.makeGameRoom(),
-    nodeA.userID,
-    "create a new game block"
+    () => miningNodeA.makeGameRoom(),
+    miningNodeA.userID,
+    "create a genesis block"
 ).then(() => {
     checkTimeSpent(
-        () => nodeB.joinGame(),
-        nodeB.userID,
-        "join a game"
-    );
+        () => miningNodeA.makeGameRoom(),
+        miningNodeA.userID,
+        "create a new game block"
+    )
+}).then(() => {
+    miningNodeA.propagateChainTemp(nodeList);
+}).then(() => {
+    gamblingNodeA.joinGame();
+}).then(() => {
+    gamblingNodeA.propagateChainTemp(nodeList);
+}).then(() => {
+    gamblingNodeB.joinGame();
+}).then(() => {
+    gamblingNodeB.propagateChainTemp(nodeList);
+}).then(() => {
+    console.log(gamblingNodeB.chain.chain.length);
+    for(let i=0; i<gamblingNodeB.chain.chain.length; i++) {
+        console.log(gamblingNodeB.chain.chain[i]);
+    }
 });
 
+// import RSAKeyGenerator from "./utils/RSAKey";
+//
+// const rsaKey = new RSAKeyGenerator();
+// const a = rsaKey.encrypt("abc");
+// console.log(a);
+// const b = rsaKey.getKeyPair();
+// console.log(b);
 
-
-
-
+// const c = rsaKey.getPublicKey();
+// console.log(c);
