e6e05f58bce67fa91ee76fe4d41326fcc680bde2
Sejin Jeon <jinaidy93@gmail.com>
2018-06-26 16:59:02 +0900

    Temporary Commit

--- test/CentralNodeStore.js
+++ test/CentralNodeStore.js
@@ -7,10 +7,41 @@ const miningNodeA = new MiningNode('mA', difficulty);
 const gamblingNodeA = new GamblingNode('gA');
 const gamblingNodeB = new GamblingNode('gB');
 
-const nodeStore = {
-    'mA': miningNodeA,
-    'gA': gamblingNodeA,
-    'gB': gamblingNodeB,
+const makeNodeStore = (numMnodes, numGnodes) => {
+    const nodeStore = {};
+    for(let i=0; i<numMnodes; i++) {
+        const nodeName = 'm' + i;
+        nodeStore[nodeName] = new MiningNode(nodeName, difficulty);
+    }
+    for(let i=0; i<numGnodes; i++) {
+        const nodeName = 'g' + i;
+        nodeStore[nodeName] = new GamblingNode(nodeName, difficulty);
+    }
+
+    return nodeStore;
 };
 
-export {nodeStore};
+const nodeStore = makeNodeStore();
+console.log(nodeStore);
+
+// export {nodeStore};
+//
+// 'use strict';
+// import MiningNode from '../MiningNode';
+// import GamblingNode from '../GamblingNode';
+//
+// const difficulty = 3;
+// const miningNodeA = new MiningNode('mA', difficulty);
+// const gamblingNodeA = new GamblingNode('gA');
+// const gamblingNodeB = new GamblingNode('gB');
+//
+//
+//
+// const nodeStore = {
+//     'mA': miningNodeA,
+//     'gA': gamblingNodeA,
+//     'gB': gamblingNodeB,
+// };
+//
+// export {nodeStore};
+//
--- web-server/server.js
+++ web-server/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import path from 'path';
 
+import {nodeStore as nodes, makeNodeStore} from "../test/CentralNodeStore";
+
 const app = express();
 const port = 80;
 const publicPath = path.resolve(__dirname, "./public");
 
 app.use(express.static(publicPath));
+app.get('/select-node', (req, res) => {
+    const numMnode = req.param('numMnode');
+    const numGnode = req.param('numGnode');
+    makeNodeStore(numMnode, numGnode);
+    console.log(nodes);
+    res.sendFile(path.resolve(publicPath, "./select-node.html"), {});
+});
 app.get('/*', (req,res) => {
     res.sendFile(path.resolve(publicPath, "./index.html"));
 });
