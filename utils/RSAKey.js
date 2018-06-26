4a22eb6eecd502ed65976dcfd57c759c6ff47dee
Sejin Jeon <jinaidy93@gmail.com>
2018-06-26 12:31:32 +0900

    Modify RSAKey.js
    Now it works.

--- utils/RSAKey.js
+++ utils/RSAKey.js
@@ -1,27 +1,45 @@
+import Buffer from 'Buffer';
+import crypto from 'crypto';
 import NodeRSA from 'node-rsa';
-
-const bitLength = 2048;
 
 export default class RSAKey {
     constructor() {
-        this.privateKey = new NodeRSA({b: bitLength});
+        const key = new NodeRSA();
+        key.generateKeyPair(2048, 65537);
+
+        this.publicKey = key.exportKey('pkcs8-public-pem');
+        this.privateKey = key.exportKey('pkcs8-private-pem');
     }
 
     getPublicKey = () => {
-        return this.privateKey.exportKey("pkcs8-public-pem");
+        return this.publicKey;
     };
 
-    encrypt = (plainText, publicKey) => {
-        const tempKey = new NodeRSA();
-        tempKey.importKey(publicKey, "pkcs8-public-pem");
-        return tempKey.encrypt(plainText, "base64");
-    };
-
-    sign = (plainText) => {
-        return this.privateKey.encrypt(plainText, "sha256");
+    encrypt = (plainText) => {
+        const enc = crypto.publicEncrypt({
+            key: this.publicKey,
+            padding: crypto.RSA_PKCS1_OAEP_PADDING
+        }, Buffer.from(plainText));
+        return enc.toString('base64');
     };
 
     decrypt = (cipherText) => {
-        return this.privateKey.decrypt(cipherText, "utf8");
+        const dec = crypto.privateDecrypt({
+            key: this.privateKey,
+            padding: crypto.RSA_PKCS1_OAEP_PADDING
+        }, Buffer.from(cipherText, 'base64'));
+        return dec.toString();
     };
+
+    sign = (plainText) => {
+        const sign = crypto.createSign('SHA256');
+        sign.update(plainText);
+        return sign.sign(this.privateKey, 'hex');
+    };
+
+    verify = (signature, plainText, publicKey) => {
+        const verify = crypto.createVerify('SHA256');
+        verify.update(plainText);
+        return verify.verify(publicKey, signature, 'hex');
+    }
 }
